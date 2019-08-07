670980d4a90be55cb936f86038034ebd66df7ab3
Tuananhno1 <anhntd14ptit@gmail.com>
2019-08-07 14:33:35 +0700

    c1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { RegisterComponent } from './register/register.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterFinalComponent,
+    RegisterComponent,
+    TemplateDrivenFormComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
